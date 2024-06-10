a59874be135819479563e6f7d995d19f2c233233
ClementMadiot <clementmadiot09@gmail.com>
2024-06-10 16:45:46 +0200

    step 1 Rock, Paper, Scissors completed

--- Rock-Paper-Scissors/scrypt.js
+++ Rock-Paper-Scissors/scrypt.js
@@ -1,5 +1,17 @@
 function getRandomComputerResult() {
-  const options = ["Rock", "Paper", "Scissors"];
+  const options = ['Rock', 'Paper', 'Scissors']
 
+  
+  for (let i = 0; i < options.length; i++) {
+    let randomNumber = Math.floor(Math.random() * options.length)
+    console.log(randomNumber)
+    if (randomNumber === 0) {
+      return options[0]
+    } else if (randomNumber === 1) {
+      return options[1]
+    } else {
+      return options[2]
+    }
+  }
 }
-console.log(getRandomComputerResult());
+console.log(getRandomComputerResult())
