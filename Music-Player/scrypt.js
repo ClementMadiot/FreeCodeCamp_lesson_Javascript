36e01a4044a2cb5c2e2d5048695d8c0e3f947963
ClementMadiot <clementmadiot09@gmail.com>
2024-07-10 14:23:27 +0200

    add reset playlist functionality + delete song

--- Music-Player/scrypt.js
+++ Music-Player/scrypt.js
@@ -5,6 +5,8 @@ const nextButton = document.getElementById("next");
 const previousButton = document.getElementById("previous");
 const shuffleButton = document.getElementById("shuffle");
 
+const playlistSongDelete = document.querySelector(".playlist-song-delete");
+
 // songs
 const allSongs = [
   {
@@ -147,6 +149,39 @@ const shuffle = () => {
   // pauseSong()
 };
 
+// delete functionality for the playlist
+const deleteSong = (id) => {
+  userData.songs = userData?.songs.filter((song) => song.id !== id);
+
+  renderSongs(userData?.songs);
+  highlightCurrentSong();
+  setPlayButtonAccessibleText();
+
+  // check if the delete song is currently play
+  if (userData?.currentSong?.id === id) {
+    userData.currentSong = null;
+    userData.songCurrentTime = 0;
+    pauseSong();
+    setPlayerDisplay();
+  }
+  // check if the playlist is empty
+  if (userData?.songs.length === 0) {
+    const resetButton = document.createElement("button");
+    const resetText = document.createTextNode("Reset Playlist");
+    resetButton.id = "reset";
+    resetButton.ariaLabel = "Reset playlist";
+    resetButton.appendChild(resetText);
+    playlistSongs.appendChild(resetButton);
+
+    resetButton.addEventListener("click", () => {
+      userData.songs = [...allSongs];
+      renderSongs(sortSongs()) // render the songs again in alphabetical order.
+      setPlayButtonAccessibleText() // update the play button accessible text
+      resetButton.remove()
+    });
+  }
+};
+console.log(userData?.songs.length);
 // display current song title & artist
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById("player-song-title");
@@ -195,7 +230,7 @@ const renderSongs = (array) => {
           <span class="playlist-song-artist">${song.artist}</span>
           <span class="playlist-song-duration">${song.duration}</span>
       </button>
-    <button class="playlist-song-delete" aria-label="Delete ${song.title}">
+    <button class="playlist-song-delete" onclick="deleteSong(${song.id})" aria-label="Delete ${song.title}">
     <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/><path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
     </button>
     </li>
@@ -226,6 +261,8 @@ nextButton.addEventListener("click", playNextSong);
 previousButton.addEventListener("click", playPreviousSong);
 
 shuffleButton.addEventListener("click", shuffle);
+
+// playlistSongDelete.addEventListener("click", deleteSong)
 
 // order song
 const sortSongs = () => {
