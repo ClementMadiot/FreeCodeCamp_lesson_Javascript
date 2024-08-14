5180ffb23a3d4245801123d1a25f10cbf192437d
ClementMadiot <clementmadiot09@gmail.com>
2024-08-14 15:32:47 +0200

    sorter algorithm smallest to largest

--- Courses/Number-sorter/script.js
+++ Courses/Number-sorter/script.js
@@ -6,14 +6,52 @@ const sortInputArray = (event) => {
     ...document.getElementsByClassName("values-dropdown"),
   ].map((dropdown) => Number(dropdown.value));
   // console.log(inputValues)
-updateUI(inputValues)
+
+  const sortedValues = selectionSort(inputValues);
+
+  updateUI(sortedValues);
 };
 
 const updateUI = (array = []) => {
   array.forEach((num, i) => {
     const outputValueNode = document.getElementById(`output-value-${i}`);
-    outputValueNode.innerText = num
+    outputValueNode.innerText = num;
   });
-}
+};
 
 sortButton.addEventListener("click", sortInputArray);
+
+// Now you need to actually sort the array. The first sorting algorithm you will implement is the bubble sort, which starts at the beginning of the array and 'bubbles up' unsorted values towards the end, iterating through the array until it is completely sorted.
+
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      // console.log(array, array[j], array[j + 1]);
+
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
+};
+
+// Time to implement another sorting algorithm. This time, you'll be implementing a selection sort. Selection sort works by finding the smallest value in the array, then swapping it with the first value in the array. Then, it finds the next smallest value in the array, and swaps it with the second value in the array. It continues iterating through the array until it is completely sorted.
+
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < array.length; j++) {
+      // console.log(array, array[j], array[minIndex]);
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+    const temp = array[i]
+    array[i] = array[minIndex]
+    array[minIndex] = temp
+  }
+  return array
+};
