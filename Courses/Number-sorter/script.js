5a0f848ca54bf5df1f4bc9f8153000bc273e9c1e
ClementMadiot <clementmadiot09@gmail.com>
2024-08-16 10:08:13 +0200

    algorith : insertion sort

--- Courses/Number-sorter/script.js
+++ Courses/Number-sorter/script.js
@@ -6,8 +6,10 @@ const sortInputArray = (event) => {
     ...document.getElementsByClassName("values-dropdown"),
   ].map((dropdown) => Number(dropdown.value));
   // console.log(inputValues)
-
-  const sortedValues = selectionSort(inputValues);
+  /// choose the algorythme you want to use by change the function bellow
+  // const sortedValues = bubbleSort(inputValues);
+  // const sortedValues = selectionSort(inputValues);
+  const sortedValues = insertionSort(inputValues);
 
   updateUI(sortedValues);
 };
@@ -21,7 +23,9 @@ const updateUI = (array = []) => {
 
 sortButton.addEventListener("click", sortInputArray);
 
-// Now you need to actually sort the array. The first sorting algorithm you will implement is the bubble sort, which starts at the beginning of the array and 'bubbles up' unsorted values towards the end, iterating through the array until it is completely sorted.
+//* algorithm : bubble sort
+
+// Starts at the beginning of the array and 'bubbles up' unsorted values towards the end, iterating through the array until it is completely sorted.
 
 const bubbleSort = (array) => {
   for (let i = 0; i < array.length; i++) {
@@ -38,10 +42,13 @@ const bubbleSort = (array) => {
   return array;
 };
 
-// Time to implement another sorting algorithm. This time, you'll be implementing a selection sort. Selection sort works by finding the smallest value in the array, then swapping it with the first value in the array. Then, it finds the next smallest value in the array, and swaps it with the second value in the array. It continues iterating through the array until it is completely sorted.
+//* algorithm : selection sort
+
+// Selection sort works by finding the smallest value in the array, then swapping it with the first value in the array. Then, it finds the next smallest value in the array, and swaps it with the second value in the array. It continues iterating through the array until it is completely sorted.
 
 const selectionSort = (array) => {
   for (let i = 0; i < array.length; i++) {
+    // minIndex = ensures that if your current value is the smallest, it will be swapped with itself and not be moved.
     let minIndex = i;
     for (let j = i + 1; j < array.length; j++) {
       // console.log(array, array[j], array[minIndex]);
@@ -49,9 +56,25 @@ const selectionSort = (array) => {
         minIndex = j;
       }
     }
-    const temp = array[i]
-    array[i] = array[minIndex]
-    array[minIndex] = temp
+    const temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
+  }
+  return array;
+};
+
+//* algorithm : insertion sort.
+// This algorithm works by building up a sorted array at the beginning of the list. It begins the sorted array with the first element. Then it inspects the next element and swaps it backward into the sorted array until it is in a sorted position, and so on.
+
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) {
+    const currValue = array[i]
+    let j = i - 1
+    while (j >= 0 && array[j] > currValue) {
+    array[j + 1] = array[j]
+    j--;
+  }
+  array[j+1] = currValue
   }
   return array
 };
