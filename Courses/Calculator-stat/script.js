1db155347330c23a321aefc8a4cdaf197bd648b3
ClementMadiot <clementmadiot09@gmail.com>
2024-08-20 08:52:42 +0200

    done the mean calculation

--- Courses/Calculator-stat/script.js
+++ Courses/Calculator-stat/script.js
@@ -1,6 +1,22 @@
+const mean = document.querySelector("#mean");
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
-  const array = []
-  array = value.split(/,\s*/g)
+  console.log(value)
+  const array = value.split(/,\s*/g)
+  // The value of an input element is always a string, even if the input type is number. 
+  const numbers = array.map(el => Number(el)).filter((el => !isNaN(el)));
+  // display the value of mean
+  mean.textContent = getMean(numbers);
 }
+
+
+const getMean = (array) => {
+  // take the sum of all numbers in the list.
+  const sum = array.reduce((acc, el) => acc + el, 0);
+  // divide the sum of numbers by the count of numbers in the list.
+  const mean = sum / array.length
+  return mean
+}
+// clean this logic up a bit. Using the implicit return 
+// const getMean = (array) => array.reduce((acc, el) => acc + el, 0)/ array.length ;
