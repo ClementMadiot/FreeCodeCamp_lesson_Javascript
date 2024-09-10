ed7510fc0cd5b87837255f726fd63a4c516e43f5
ClementMadiot <clementmadiot09@gmail.com>
2024-09-10 11:24:52 +0200

    add the update with all condition's player

--- Courses/Platformer_game/script.js
+++ Courses/Platformer_game/script.js
@@ -12,35 +12,44 @@ canvas.height = innerHeight;
 const gravity = 0.5;
 let isCheckpointCollisionDetectionActive = true;
 
-
 // size of element
 const proportionalSize = (size) => {
- return innerHeight < 500 ? Math.ceil((size / 500) * innerHeight) : size;
-}
+  return innerHeight < 500 ? Math.ceil((size / 500) * innerHeight) : size;
+};
 
 // characteristics of the player
 class Player {
   constructor() {
-// the proportionalSize function make sure that the player's position is always proportional to the screen size. 
+    // the proportionalSize function make sure that the player's position is always proportional to the screen size.
     this.position = {
       x: proportionalSize(10),
-      y: proportionalSize(400)
-    }
+      y: proportionalSize(400),
+    };
     this.velocity = {
       x: 0,
-      y: 0
-    }
-// The velocity property store the player's speed in the x and y directions.
+      y: 0,
+    };
+    // The velocity property store the player's speed in the x and y directions.
     this.width = proportionalSize(40);
     this.height = proportionalSize(40);
-// proportionalSize() function set the width & height properties of your class to be proportional to the height of the screen.
+    // proportionalSize() function set the width & height properties of your class to be proportional to the height of the screen.
   }
   draw() {
-    ctx.fillStyle = '#99c9ff'
+    ctx.fillStyle = "#99c9ff";
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
   update() {
     this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      if(this.position.y < 0){
+        this.position.y = 0
+        this.velocity.y = gravity
+      }
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
   }
 }
-
