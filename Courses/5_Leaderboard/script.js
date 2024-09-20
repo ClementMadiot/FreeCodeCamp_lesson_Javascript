7e8eafe0a86a313cad9042f1147ba6138116005d
ClementMadiot <clementmadiot09@gmail.com>
2024-09-20 11:21:13 +0200

    config time + display post

--- Courses/5_Leaderboard/script.js
+++ Courses/5_Leaderboard/script.js
@@ -8,6 +8,22 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 // get elements
 const postsContainer = document.getElementById("posts-container");
 
+const timeAgo = (time) => {
+  const currentTime = new Date(); // current date and time
+  const lastPost = new Date(time); // date of the last activity on the topic
+
+  let minutes = Math.floor((currentTime - lastPost) / 60000); // difference in minutes
+  let hours = Math.floor((currentTime - lastPost) / 3600000); // difference in hours
+  let days = Math.floor((currentTime - lastPost) / 86400000); // difference in days
+  if (minutes < 60) {
+    return `${minutes}m ago`;
+  } else if (hours < 24) {
+    return `${hours}h ago`;
+  } else {
+    return `${days}d ago`;
+  }
+};
+
 const fetchData = async () => {
   try {
     const res = await fetch(forumLatest);
@@ -39,19 +55,17 @@ const showLatestPosts = (data) => {
         category_id,
         bumped_at,
       } = item;
-      return `
-    <tr>
-    <td>
-    <p class="post-title">${title}</p>
-    </td>
-    <td></td>
-    <td>
-    ${posts_count - 1}
-    </td>
-    <td>${views}</td>
-    <td></td>
-    </tr>
-    `;
+      return html`
+        <tr>
+          <td>
+            <p class="post-title">${title}</p>
+          </td>
+          <td></td>
+          <td>${posts_count - 1}</td>
+          <td>${views}</td>
+          <td>${timeAgo(bumped_at)}</td>
+        </tr>
+      `;
     })
     .join("");
 };
