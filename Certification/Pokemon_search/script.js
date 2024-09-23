b1fbc47192f16cdc9e0463c64ee6cdc98341428c
ClementMadiot <clementmadiot09@gmail.com>
2024-09-23 11:39:28 +0200

    add styles + html

---
+++ Certification/Pokemon_search/script.js
@@ -0,0 +1,18 @@
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+// info pokemon 
+const pokemonName = document.getElementById('pokemon-name');
+const pokemonId = document.getElementById('pokemon-id');  
+const weight = document.getElementById('weight');
+const height = document.getElementById('height');
+const imgContainer = document.getElementsByClassName('img-container');
+const pokemonImage = document.getElementById('pokemon-image');
+const types = document.getElementById('types');
+//stats pokemon 
+const hp = document.getElementById('hp');
+const attack = document.getElementById('attack');
+const defense = document.getElementById('defense');
+const specialAttack = document.getElementById('special-attack');
+const specialDefense = document.getElementById('special-defense');
+const speed = document.getElementById('speed');
+
